1a45c5fb43e8b19ca290db2867cf384aaf762739
Calpa Liu <calpaliu@gmail.com>
2025-06-14 08:01:42 +0800

    feat: add slide-bottom animation component with fade transition effects

--- slide-bottom.vue
+++ slide-bottom.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="flex justify-between absolute bottom-0 w-full p-1 z-10">
-    <div class="text-xs text-white/60">
+  <div class="flex justify-between absolute bottom-0 w-full p-1 z-10"
+    v-if="$nav.currentPage !== 1">
+    <div class="text-xs text-white/40">
       ことばと式で、あたらしい現実を錬成する。
     </div>
     <div
-      class="text-xs text-white/60 text-right"
+      class="text-xs text-white/40 text-right"
     >
       © Calpa 的煉金工房. All rights reserved.
     </div>
