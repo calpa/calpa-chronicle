e11203db4c9fee1e26e8e559c4fd84803590bf8a
calpa <calpaliu@gmail.com>
2025-07-07 15:00:07 +0800

    docs: add July 10th chronicle entry and slide-bottom animation component

--- slide-bottom.vue
+++ slide-bottom.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="flex justify-between absolute bottom-0 w-full p-1 z-10"
-    v-if="$nav.currentPage !== 1">
+  <div
+    class="flex justify-between absolute bottom-0 w-full p-1 z-10"
+    v-if="$nav.currentPage !== 1"
+  >
     <div class="text-xs text-white/40">
       ことばと式で、あたらしい現実を錬成する。
     </div>
-    <div
-      class="text-xs text-white/40 text-right"
-    >
-      © Calpa 的煉金工房. All rights reserved.
+    <div class="text-xs text-white/40 text-right">
+      {{
+        $slidev.configs.footerRightText ||
+        "© Calpa 的煉金工房. All rights reserved."
+      }}
     </div>
   </div>
 </template>
